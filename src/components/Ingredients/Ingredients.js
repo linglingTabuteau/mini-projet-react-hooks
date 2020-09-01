a6f4cbb31a5657a1a7a335c48d3b448ce233701c
Lingling Tabuteau <lingling.tabuteau@gmail.com>
2020-09-01 19:33:12 +0200

    add&remove userIngredients

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList'
+import Search from './Search';
+
+const Ingredients = () => {
+
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientsHandler = (ingredient) => {
+    setUserIngredients(preUserIngredients => [...preUserIngredients, { id: Math.random().toString(), ...ingredient }])
+  }
+
+  const onRemoveItemHandler = (id) => {
+    let newUserIngredients = userIngredients.filter(userIngredient => userIngredient.id !== id)
+    setUserIngredients(newUserIngredients);
+  }
+
+  return (
+    <div className="App">
+      <IngredientForm
+        addIngredients={addIngredientsHandler} />
+
+      <section>
+        <Search />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={(id) => onRemoveItemHandler(id)} />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
